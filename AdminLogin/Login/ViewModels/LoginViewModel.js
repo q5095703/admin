407ad6e13d962852e1b4200427cf6335623a1e04
q5095703 <q5095703@gmail.com>
2018-11-22 17:26:15 +0000

    Add files via upload

---
+++ AdminLogin/Login/ViewModels/LoginViewModel.js
@@ -0,0 +1,27 @@
+﻿
+$(document).ready(function () {
+    var viewModel = function () {
+        debugger;
+        var self = this;
+        self.email = ko.observable("bbo");
+        self.password = ko.observable("vv");
+        self.save = function () {
+            debugger;
+            $.ajax({
+                url: "/Home/Login",
+                type: "POST",
+                data: {
+                    email: self.email(),
+                    password: self.password()
+                }
+            })
+        }
+
+    }
+
+    ko.applyBindings(new viewModel())
+
+
+    // executes when HTML-Document is loaded and DOM is ready
+    alert("document is ready");
+});
